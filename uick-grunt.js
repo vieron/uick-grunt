1e2a4b57619a711ae8e6a621a283868682b614e3
vieron <javiersanchezmarin@gmail.com>
2013-09-21 22:52:18 +0200

    unified spacing and fixed concat task

--- uick-grunt.js
+++ uick-grunt.js
@@ -11,117 +11,119 @@
         var pkg = grunt.file.readJSON('package.json');
         var component = {};
         component[pkg.name] = {
-          output: './build/',
-          styles: true,
-          scripts: true,
-          verbose: true,
-          configure: function(builder) {
-            builder.use(sass);
-          }
+            output: './build/',
+            styles: true,
+            scripts: true,
+            verbose: true,
+            configure: function(builder) {
+                builder.use(sass);
+            }
         };
 
         component[pkg.name + '-standalone'] =  {
-          output: './build/',
-          styles: false,
-          scripts: true,
-          verbose: true,
-          noRequire: true,
-          standalone: true
+            output: './build/',
+            styles: false,
+            scripts: true,
+            verbose: true,
+            noRequire: true,
+            standalone: true
         };
 
 
         // Project configuration.
         grunt.initConfig({
-          pkg: pkg,
-          components: grunt.file.readJSON('component.json'),
-          meta: {
-            banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
-                    '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-                    '<%= pkg.homepage ? "* " + pkg.homepage : "" %>\n' +
-                    '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-                    ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n\n',
+            pkg: pkg,
+            components: grunt.file.readJSON('component.json'),
+            meta: {
+                banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+                        '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+                        '<%= pkg.homepage ? "* " + pkg.homepage : "" %>\n' +
+                        '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+                        ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n\n',
 
-            minibanner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-                        '<%= grunt.template.today("yyyy-mm-dd") %> - ' +
-                        '<%= pkg.homepage ? "* " + pkg.homepage + " - " : "" %>' +
-                        'Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-                        ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */ '
-          },
-          uglify: {
-            options: {
-              banner: '<%= meta.minibanner %>\n'
+                minibanner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+                            '<%= grunt.template.today("yyyy-mm-dd") %> - ' +
+                            '<%= pkg.homepage ? "* " + pkg.homepage + " - " : "" %>' +
+                            'Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+                            ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */ '
             },
-            files: {
-              'build/<%= pkg.name %>.min.js': ['build/<%= pkg.name %>.js'],
-              'build/<%= pkg.name %>-standalone.min.js': ['build/<%= pkg.name %>-standalone.js']
-            }
-          },
-          cssmin: {
-            compress: {
+            uglify: {
               options: {
-                keepSpecialComments: 0,
-                banner: '<%= meta.minibanner %>\n'
+                  banner: '<%= meta.minibanner %>\n'
               },
-              files: {
-                "build/<%= pkg.name %>.min.css": ["build/<%= pkg.name %>.css"]
+              min: {
+                  files: {
+                    'build/<%= pkg.name %>.min.js': ['build/<%= pkg.name %>.js'],
+                    'build/<%= pkg.name %>-standalone.min.js': ['build/<%= pkg.name %>-standalone.js']
+                  }
               }
-            }
-          },
-
-          component_build: component,
-
-          concat: {
-            css:{
-              options: {
-                separator: ';',
-                banner: "<%= meta.minibanner %>\n"
-              },
-              files: {
-                'build/<%= pkg.name %>.css': ['build/<%= pkg.name %>.css']
-              },
             },
-            component:{
-              options: {
-                separator: ';',
-                banner: "<%= meta.minibanner %>\n"
-              },
-              files: {
-                'build/<%= pkg.name %>.js': ['build/<%= pkg.name %>.js']
-              },
+            cssmin: {
+              compress: {
+                  options: {
+                      keepSpecialComments: 0,
+                      banner: '<%= meta.minibanner %>\n'
+                  },
+                  files: {
+                      "build/<%= pkg.name %>.min.css": ["build/<%= pkg.name %>.css"]
+                  }
+              }
             },
-            standalone: {
-              options: {
-                separator: ';',
-                banner: "<%= meta.minibanner %>\n",
-                // banner: "Uick.register([\"<%= _.map(_.keys(components.dependencies), function(c){ return c.split('/')[1].replace('ui-', ''); }).join('\", \"') %>\"]);\n",
-                process: function(src, filepath) {
-                  return src + "\n\nuick.register([\"" + _.map(_.keys(grunt.config('components').dependencies), function(c){ return c.split('/')[1].replace('ui-', ''); }).join('\", \"') + "\"]);\n"
-                }
-              },
-              files: {
-                'build/<%= pkg.name %>-standalone.js': ['build/<%= pkg.name %>-standalone.js']
-              },
-            },
-          },
 
-          mocha: {
-            uick: {
-              src: ['tests/index.html'],
-              options: {
-                mocha: {
-                  ignoreLeaks: false
+            component_build: component,
+
+            concat: {
+                css:{
+                    options: {
+                        separator: ';',
+                        banner: "<%= meta.banner %>\n"
+                    },
+                    files: {
+                        'build/<%= pkg.name %>.css': ['build/<%= pkg.name %>.css']
+                    },
                 },
-                reporter: 'Spec',
-                // Indicates whether 'mocha.run()' should be executed in 'bridge.js'
-                run: true
-              }
-            }
-          },
+                component:{
+                    options: {
+                        separator: ';',
+                        banner: "<%= meta.banner %>\n"
+                    },
+                    files: {
+                        'build/<%= pkg.name %>.js': ['build/<%= pkg.name %>.js']
+                    },
+                },
+                standalone: {
+                    options: {
+                        separator: ';',
+                        banner: "<%= meta.banner %>\n",
+                        // banner: "Uick.register([\"<%= _.map(_.keys(components.dependencies), function(c){ return c.split('/')[1].replace('ui-', ''); }).join('\", \"') %>\"]);\n",
+                        process: function(src, filepath) {
+                            return src + "\n\nuick.register([\"" + _.map(_.keys(grunt.config('components').dependencies), function(c){ return c.split('/')[1].replace('ui-', ''); }).join('\", \"') + "\"]);\n"
+                        }
+                    },
+                    files: {
+                        'build/<%= pkg.name %>-standalone.js': ['build/<%= pkg.name %>-standalone.js']
+                    },
+                },
+            },
 
-          watch: {
-            files: ['index.js', 'templates/*.html', 'index.scss', 'Gruntfile.js'],
-            tasks: ['component_build', 'concat', 'uglify', 'cssmin']
-          }
+            mocha: {
+                uick: {
+                    src: ['tests/index.html'],
+                    options: {
+                        mocha: {
+                            ignoreLeaks: false
+                        },
+                        reporter: 'Spec',
+                        // Indicates whether 'mocha.run()' should be executed in 'bridge.js'
+                        run: true
+                    }
+                }
+            },
+
+            watch: {
+                files: ['index.js', 'templates/*.html', 'index.scss', 'Gruntfile.js'],
+                tasks: ['component_build', 'concat', 'uglify', 'cssmin']
+            }
         });
 
         // Load the plugin that provides the "uglify" task.
